85db7d73610fb1a4e00c22ef721a2bc3c4e52e8e
Lawrence Mainja <lmainja@udel.edu>
2018-10-12 13:57:37 -0500

    changes to the about page

--- creek/users/forms.py
+++ creek/users/forms.py
@@ -8,7 +8,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username']
 
 class UserUpdateForm(forms.ModelForm):
      emails = forms.EmailField()
